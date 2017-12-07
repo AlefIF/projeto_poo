15a1cce57e6a1098eba777495f51c5fe0d0ed90c
8Alef8 <aleffaria99@gmail.com>
2017-12-07 16:46:02 -0200

    Passando pra hiber

--- TechSales/src/Visao/RelatorioDeVendas.java
+++ TechSales/src/Visao/RelatorioDeVendas.java
@@ -12,9 +12,9 @@ import javax.swing.JTextField;
  *
  * @author polo
  */
-public class ExemploRelatorio2 extends javax.swing.JFrame {
+public class RelatorioDeVendas extends javax.swing.JFrame {
 
-    public ExemploRelatorio2() {
+    public RelatorioDeVendas() {
         initComponents();
         setSize(400, 280);
 
@@ -124,20 +124,20 @@ public class ExemploRelatorio2 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ExemploRelatorio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RelatorioDeVendas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ExemploRelatorio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RelatorioDeVendas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ExemploRelatorio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RelatorioDeVendas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ExemploRelatorio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RelatorioDeVendas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ExemploRelatorio2().setVisible(true);
+                new RelatorioDeVendas().setVisible(true);
             }
         });
     }
